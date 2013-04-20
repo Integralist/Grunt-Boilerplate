aefeae2344a300290909d8193120a1adc7a381a6
Mark McDonnell <mark.mcdx@gmail.com>
2013-04-20 23:44:57 +0100

    Add Mock Example

--- specs/SinonSpec.js
+++ specs/SinonSpec.js
@@ -6,7 +6,8 @@ describe('Sinon.js', function(){
             },
             add10: function(num) {
                 return num + 10;
-            }
+            },
+            delegation: function(){ this.doSomething(); }
         };
     });
 
@@ -27,5 +28,16 @@ describe('Sinon.js', function(){
         
         this.obj.doSomething.restore();
         this.obj.add10.restore();
+    });
+
+    it('should demonstate mock feature', function(){
+        var mock = sinon.mock(this.obj);
+            mock.expects('doSomething').exactly(2);
+
+        this.obj.delegation('Some Message');
+        this.obj.doSomething('Some Other Message');
+
+        mock.verify();
+        mock.restore();
     });
 });
